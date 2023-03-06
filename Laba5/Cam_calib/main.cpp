d901e2cde286e9e3b991539609fad427b12d9ee9
Viktq <90345098+Viktq@users.noreply.github.com>
2023-03-06 21:37:22 +0300

    3/4 лабы... В один коммит не влезло

---
+++ Laba5/Cam_calib/main.cpp
@@ -0,0 +1,176 @@
+/*
+By downloading, copying, installing or using the software you agree to this
+license. If you do not agree to this license, do not download, install,
+copy or use the software.
+
+                          License Agreement
+               For Open Source Computer Vision Library
+                       (3-clause BSD License)
+
+Copyright (C) 2013, OpenCV Foundation, all rights reserved.
+Third party copyrights are property of their respective owners.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+  * Neither the names of the copyright holders nor the names of the contributors
+    may be used to endorse or promote products derived from this software
+    without specific prior written permission.
+
+This software is provided by the copyright holders and contributors "as is" and
+any express or implied warranties, including, but not limited to, the implied
+warranties of merchantability and fitness for a particular purpose are
+disclaimed. In no event shall copyright holders or contributors be liable for
+any direct, indirect, incidental, special, exemplary, or consequential damages
+(including, but not limited to, procurement of substitute goods or services;
+loss of use, data, or profits; or business interruption) however caused
+and on any theory of liability, whether in contract, strict liability,
+or tort (including negligence or otherwise) arising in any way out of
+the use of this software, even if advised of the possibility of such damage.
+*/
+
+
+#include <opencv2/highgui.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/aruco.hpp>
+#include <opencv2/imgproc.hpp>
+#include <vector>
+#include <iostream>
+#include <ctime>
+#include "aruco_samples_utility.h"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char *argv[]) {
+
+    int markersX = 7;
+    int markersY = 10;
+    float markerLength = 50;
+    float markerSeparation = 25;
+    int margins = 25;
+    int dict = cv::aruco::DICT_5X5_1000;
+    int borderBits = 1;
+    string outputFile = "C:\\WIP\\Tex3penie\\Laba_5_Clion_2\\Cam_calib\\calibration.yml";
+
+    int calibrationFlags = 0;
+    float aspectRatio = 1.33;
+    bool zt = true;
+    bool pc = false;
+
+    if(aspectRatio != 1.0f) {
+        calibrationFlags |= CALIB_FIX_ASPECT_RATIO;
+    }
+    if(zt) calibrationFlags |= CALIB_ZERO_TANGENT_DIST;
+    if(pc) calibrationFlags |= CALIB_FIX_PRINCIPAL_POINT;
+
+    Ptr<aruco::DetectorParameters> detectorParams = aruco::DetectorParameters::create();
+
+
+    bool refindStrategy = false;
+    int camId = 0;
+    String video;
+
+    VideoCapture inputVideo;
+    int waitTime;
+    if(!video.empty()) {
+        inputVideo.open(video);
+        waitTime = 0;
+    } else {
+        inputVideo.open(camId);
+        waitTime = 10;
+    }
+
+    Ptr<aruco::Dictionary> dictionary = aruco::getPredefinedDictionary(dict);
+
+    // create board object
+    Ptr<aruco::GridBoard> gridboard =
+            aruco::GridBoard::create(markersX, markersY, markerLength, markerSeparation, dictionary);
+    Ptr<aruco::Board> board = gridboard.staticCast<aruco::Board>();
+
+    // collected frames for calibration
+    vector< vector< vector< Point2f > > > allCorners;
+    vector< vector< int > > allIds;
+    Size imgSize;
+
+    while(inputVideo.grab()) {
+        Mat image, imageCopy;
+        inputVideo.retrieve(image);
+
+        vector< int > ids;
+        vector< vector< Point2f > > corners, rejected;
+
+        // detect markers
+        aruco::detectMarkers(image, dictionary, corners, ids, detectorParams, rejected);
+
+        // refind strategy to detect more markers
+        if(refindStrategy) aruco::refineDetectedMarkers(image, board, corners, ids, rejected);
+
+        // draw results
+        image.copyTo(imageCopy);
+        if(ids.size() > 0) aruco::drawDetectedMarkers(imageCopy, corners, ids);
+        putText(imageCopy, "Press 'c' to add current frame. 'ESC' to finish and calibrate",
+                Point(10, 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 0, 0), 2);
+
+        imshow("out", imageCopy);
+        char key = (char)waitKey(waitTime);
+        if(key == 27) break;
+        if(key == 'c' && ids.size() > 0) {
+            cout << "Frame captured" << endl;
+            allCorners.push_back(corners);
+            allIds.push_back(ids);
+            imgSize = image.size();
+        }
+    }
+
+    if(allIds.size() < 1) {
+        cerr << "Not enough captures for calibration" << endl;
+        return 0;
+    }
+
+    Mat cameraMatrix, distCoeffs;
+    vector< Mat > rvecs, tvecs;
+    double repError;
+
+    if(calibrationFlags & CALIB_FIX_ASPECT_RATIO) {
+        cameraMatrix = Mat::eye(3, 3, CV_64F);
+        cameraMatrix.at< double >(0, 0) = aspectRatio;
+    }
+
+    // prepare data for calibration
+    vector< vector< Point2f > > allCornersConcatenated;
+    vector< int > allIdsConcatenated;
+    vector< int > markerCounterPerFrame;
+    markerCounterPerFrame.reserve(allCorners.size());
+    for(unsigned int i = 0; i < allCorners.size(); i++) {
+        markerCounterPerFrame.push_back((int)allCorners[i].size());
+        for(unsigned int j = 0; j < allCorners[i].size(); j++) {
+            allCornersConcatenated.push_back(allCorners[i][j]);
+            allIdsConcatenated.push_back(allIds[i][j]);
+        }
+    }
+    // calibrate camera
+    repError = aruco::calibrateCameraAruco(allCornersConcatenated, allIdsConcatenated,
+                                           markerCounterPerFrame, board, imgSize, cameraMatrix,
+                                           distCoeffs, rvecs, tvecs, calibrationFlags);
+
+    bool saveOk = saveCameraParams(outputFile, imgSize, aspectRatio, calibrationFlags, cameraMatrix,
+                                   distCoeffs, repError);
+
+    if(!saveOk) {
+        cerr << "Cannot save output file" << endl;
+        return 0;
+    }
+
+    cout << "Rep Error: " << repError << endl;
+    cout << "Calibration saved to " << outputFile << endl;
+
+    return 0;
+}
